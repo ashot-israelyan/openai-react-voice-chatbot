b9e0e439d447f7ab19bd9ff4af1c054951a2f181
Ashot Israelyan <ashot@picsart.com>
2023-06-30 13:19:52 +0400

    implemented frontend

--- backend/functions/database.py
+++ backend/functions/database.py
@@ -1,48 +1,50 @@
+import os
 import json
 import random
 
-# Get recent messages
+# Save messages for retrieval later on
 def get_recent_messages():
 
-    # Define the file name and learn instruction
-    file_name = "stored_data.json"
-    learn_instruction = {
-        "role": "system",
-        "content": "You are interviewing the user for a job as a retail assistant. Ask short question that are relevant to the junior position. Your name is Rachel. The user is called Shaun. Keep you answers to under 30 words.",
-    }
+  # Define the file name
+  file_name = "stored_data.json"
+  learn_instruction = {"role": "system",
+                       "content": "You are a English teacher and your name is Rachel, the user is called Shaun. Keep responses under 20 words. "}
 
-    # Initialize messages
-    messages = []
+  # Initialize messages
+  messages = []
 
-    # Add a random element
-    x = random.uniform(0, 1)
-    if x < 0.5:
-        learn_instruction["content"] = learn_instruction["content"] + " Your response will include some dry humour."
-    else:
-        learn_instruction["content"] = learn_instruction["content"] + " Your response will include a rather challenging question."
+  # Add Random Element
+  x = random.uniform(0, 1)
+  if x < 0.2:
+    learn_instruction["content"] = learn_instruction["content"] + "Your response will have some light humour. "
+  elif x < 0.5:
+    learn_instruction["content"] = learn_instruction["content"] + "Your response will include an interesting new fact about USA. "
+  else:
+    learn_instruction["content"] = learn_instruction["content"] + "Your response will recommend another word to learn. "
 
-    # Append instruction to message
-    messages.append(learn_instruction)
+  # Append instruction to message
+  messages.append(learn_instruction)
 
-    # Get last messages
-    try:
-        with open(file_name) as user_file:
-            data = json.load(user_file)
+  # Get last messages
+  try:
+    with open(file_name) as user_file:
+      data = json.load(user_file)
 
-            # Append last 5 items of data
-            if data:
-                if len(data) < 5:
-                    for item in data:
-                        messages.append(item)
-                else:
-                    for item in data[-5]:
-                        messages.append(item)
-    except Exception as e:
-        print(e)
-        pass
+      # Append last 5 rows of data
+      if data:
+        if len(data) < 5:
+          for item in data:
+            messages.append(item)
+        else:
+          for item in data[-5:]:
+            messages.append(item)
+  except:
+    pass
 
-    # Return
-    return messages
+
+  # Return messages
+  return messages
+
 
 # Save messages for retrieval later on
 def store_messages(request_message, response_message):
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
+import Controller from './components/Controller';
+
 function App() {
-  return (
-    <div className="">
-      <div className="text-2xl bg-blue-500">Hello</div>
-    </div>
-  );
+  return <Controller />;
 }
 
 export default App;
